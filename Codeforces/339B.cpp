fc356ca8c47fbc02e92e64f3564c5eb9a730021b
ssaraf0702 <63305072+ssaraf0702@users.noreply.github.com>
2021-12-25 15:21:46 +0530

    Day 15

--- Codeforces/339B.cpp
+++ Codeforces/339B.cpp
@@ -1,27 +1,29 @@
-#include <cstdio>
-#include <iostream>
+#include<iostream>
+typedef long long ll;
 using namespace std;
 
 int main()
 {
-    int n, m;
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ll n,m;
+    ll l=1;
     cin >> n >> m;
-
-    int loc = 1;
-    long long int sum = 0;
+    ll sum=0;
     for(int i=0; i<m; i++)
     {
         int now;
         cin >> now;
-
-        if(now >= loc)
-            sum += now - loc;
-        else
-            sum += n - (loc - now);
-        loc = now;
+        if(now >= l)
+        {
+            sum = sum + (now-l);
+            l=now;
+        }
+        else{
+           sum = sum + n -(l-now);
+           l = now;
+        }
     }
 
-    cout << sum << endl;
-
+    cout << sum <<endl;
     return 0;
 }
